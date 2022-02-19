51f09088eaaf04bd37144560dc4413386ac79ca7
Henry Allen <hallen@roblox.com>
2022-02-18 18:41:27 -0800

    Test react-window

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import ReactVirtualizedExample from "./ReactVirtualizedExample/index"
+import ReactVirtualizedExample from "./ReactVirtualizedExample"
+import ReactWindowExample from './ReactWindowExample'
 
 function App() {
   return (
     <div className="App">
-      <ReactVirtualizedExample />
+      <div>
+        <h1>React-Virtualized</h1>
+        <ReactVirtualizedExample />
+      </div>
+      <div>
+        <h1>React-Window</h1>
+        <ReactWindowExample />
+      </div>
     </div>
   );
 }
---
+++ src/ReactWindowExample/index.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { FixedSizeList as List } from "react-window"
+
+const initialList = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k" ]
+
+
+function ReactWindowExample() {
+	const [list, setList] = React.useState(initialList)
+	const loadNextPage = () => {
+		setList((oldList) => {
+			return oldList.concat([
+				"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"
+			])
+		})
+	}
+
+	return (
+		<div>
+			<List
+				height={150}
+				itemCount={list.length}
+				itemSize={35}
+				width={300}>
+					{
+						({ index, style }) => {
+							if (list.length - index < 20) {
+								loadNextPage()
+							}
+							return (
+								<div style={style}>{list[index]}</div>
+							)
+						}
+					}
+			</List>
+		</div>
+	)
+}
+
+export default ReactWindowExample
